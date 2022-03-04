f35afd7afb5571824f39b28fcc2d177148202c06
werick souza <werickxsilva@gmail.com>
2022-03-04 14:17:08 -0300

    compilando usando cargo

---
+++ test_cargo/target/debug/deps/test_cargo-3755203e11931e23.d
@@ -0,0 +1,5 @@
+/home/werick/Documentos/Repositorios/github/RustProjects/test_cargo/target/debug/deps/test_cargo-3755203e11931e23: src/main.rs
+
+/home/werick/Documentos/Repositorios/github/RustProjects/test_cargo/target/debug/deps/test_cargo-3755203e11931e23.d: src/main.rs
+
+src/main.rs:
---
+++ test_cargo/target/debug/test_cargo.d
@@ -0,0 +1,1 @@
+/home/werick/Documentos/Repositorios/github/RustProjects/test_cargo/target/debug/test_cargo: /home/werick/Documentos/Repositorios/github/RustProjects/test_cargo/src/main.rs
