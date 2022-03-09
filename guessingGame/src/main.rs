c0c00ed90ef27c49c6559bc896b2acc5167de954
werick souza <werickxsilva@gmail.com>
2022-03-09 17:25:49 -0300

    feat (class) : Gerando um Numeor aleatorio
    utilizando a dependencia rand para gerar um numero aleatorio e armazenando-o em uma variavel imutavel

--- guessingGame/src/main.rs
+++ guessingGame/src/main.rs
@@ -1,15 +1,19 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
-    println!("Adivinhe um número! ");
+    println!("Adivinhe um número!");
 
-    println!("Digite um número: ");
-    let mut chute= String::new();
+    let numero_secreto = rand::thread_rng().gen_range(1..101);
+    println!("O número secreto é: {}", numero_secreto);
+
+    println!("Digite o palpite de um número : ");
+    let mut palpite_numero = String::new();
 
     io::stdin()
-        .read_line(&mut chute )
+        .read_line(&mut palpite_numero)
         .expect("Falha ao ler a linha");
 
-    println!("Seu chute foi: {} ", chute);
+    println!("Seu palpite foi: {} ", palpite_numero);
 
 }
