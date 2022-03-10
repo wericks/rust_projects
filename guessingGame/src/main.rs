ffabe7953b8f50497dc12946a844efc5259fbd8c
werick souza <werickxsilva@gmail.com>
2022-03-10 11:55:25 -0300

    feat (class) : lidando com erros e loop
    Lidando com possiveis erros causados pelo usuario,
    Loop para que seja possivel a continuidade do jogo apos insercoes do ususario

--- guessingGame/src/main.rs
+++ guessingGame/src/main.rs
@@ -7,26 +7,38 @@ fn main() {
     println!("Adivinhe um número!\n");
 
     let numero_secreto = rand::thread_rng().gen_range(1..101);
-    println!("O número secreto é: {}", numero_secreto);
+    // println!("O número secreto é: {}", numero_secreto);
 
-    println!("Digite o palpite de um número : ");
-    let mut palpite_numero = String::new();
+    // loop para permitir mais de uma única tentativa
+    loop {
+        println!("Digite o palpite de um número : ");
+        let mut palpite_numero = String::new();
 
-    // lidando com possiveis falhas
-    io::stdin()
-        .read_line(&mut palpite_numero)
-        .expect("Falha ao ler a linha");
+        // lidando com possiveis falhas
+        io::stdin()
+            .read_line(&mut palpite_numero)
+            .expect("Falha ao ler a linha");
 
-    // sombrea o palpite_numero e passa o seu valor string para u32
-    let palpite_numero: u32 = palpite_numero.trim().parse().expect("Por favor insira um número!");
+        // sombrea o palpite_numero e passa o seu valor string para u32
+        // match para lidar com error
+        let palpite_numero: u32 = match palpite_numero.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("Seu palpite foi: {} ", palpite_numero);
+        // implementação obsoleta com a utilização de expect e crashing par atipos diferentes
+       // let palpite_numero: u32 = palpite_numero.trim().parse().expect("Por favor insira um número!");
 
-    // utiliza o numero_secreto para selecionar um dos arms
-    match palpite_numero.cmp(&numero_secreto) {
-        Ordering::Less => println!("muito baixo!"),
-        Ordering::Greater => println!("muito alto!"),
-        Ordering::Equal => println!("Você Venceu!!!"),
+        println!("Seu palpite foi: {} ", palpite_numero);
+
+        // utiliza o numero_secreto para selecionar um dos arms
+        match palpite_numero.cmp(&numero_secreto) {
+            Ordering::Less => println!("muito baixo!"),
+            Ordering::Greater => println!("muito alto!"),
+            Ordering::Equal => {
+                println!("Você Venceu!!!");
+                break;
+            }
+        }
     }
-
 }
