7d554bda1085ce7c1cc31329e43f683cb13837fe
werick souza <werickxsilva@gmail.com>
2022-03-12 12:33:23 -0300

    feat (class) : poluindo com conceitos
    polui um pouco o código do jogo para testar alguns conceitos simples, sobre tuplas, operadores aritméticos

--- guessingGame/src/main.rs
+++ guessingGame/src/main.rs
@@ -8,6 +8,29 @@ fn main() {
 
     let numero_secreto = rand::thread_rng().gen_range(1..101);
     // println!("O número secreto é: {}", numero_secreto);
+    let soma = 32 + 9;
+    let resto = 40 % 6;
+    println!("soma {}\nresto divisão: {}", soma, resto);
+
+    let tup : (i32, f64, u8) = (500, 6.4, 2);
+        // acesando tupla atravez de destructing
+        let (x, y, z) = tup;
+        println!("\nvalor Z na tupla : {} ", z);
+        println!("\nvalor Y na tupla : {} ", y);
+        println!("\nvalor X na tupla : {} \n", x);
+
+    // acessando tupla atravez do elemento e index
+    let quinhentos = tup.0;
+    let seis_quatro = tup.1;
+    let dois = tup.2;
+        println!("\nvalor X na tupla : {} \n", quinhentos);
+        println!("\nvalor X na tupla : {} \n", seis_quatro);
+        println!("\nvalor X na tupla : {} \n", dois);
+
+
+
+
+
 
     // loop para permitir mais de uma única tentativa
     loop {
