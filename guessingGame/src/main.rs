0b469b996647d996ec8d0ccc40a5b99b88773841
werick souza <werickxsilva@gmail.com>
2022-03-13 09:42:20 -0300

    feat (class) : utilizando metodos

--- guessingGame/src/main.rs
+++ guessingGame/src/main.rs
@@ -4,68 +4,90 @@ use std::cmp::Ordering;
 
 
 fn main() {
-    // println!("Adivinhe um número!\n");
+    array_teste();
+}
 
-    // let numero_secreto = rand::thread_rng().gen_range(1..101);
-    // println!("O número secreto é: {}", numero_secreto);
+fn array_teste() {
+    // exibindo arrays
+    println!("Arrays");
+      let array = [2, 4, 55, 6];
+      let mut index = String::new();
 
+      io::stdin()
+          .read_line(&mut index)
+          .expect("Falha em ler a linha");
 
+      let index: usize = index
+          .trim()
+          .parse()
+          .expect("Index infoormado não encontrado");
 
+      let elemento = array[index];
+
+      println!("o valor do elemento no index {} é {}", index, elemento);
+}
+
+fn guessing_game() {
+
+    println!("Adivinhe um número!\n");
+
+    let numero_secreto = rand::thread_rng().gen_range(1..101);
+    println!("O número secreto é: {}", numero_secreto);
+
+    // loop para permitir mais de uma única tentativa
+      loop {
+
+          println!("Digite o palpite de um número : ");
+          let mut palpite_numero = String::new();
+
+          // lidando com possiveis falhas
+          io::stdin()
+              .read_line(&mut palpite_numero)
+              .expect("Falha ao ler a linha");
+
+          // sombrea o palpite_numero e passa o seu valor string para u32
+          // match para lidar com error
+          let palpite_numero: u32 = match palpite_numero.trim().parse() {
+              Ok(num) => num,
+              Err(_) => continue,
+          };
+
+          // implementação obsoleta com a utilização de expect e crashing par atipos diferentes
+         // let palpite_numero: u32 = palpite_numero.trim().parse().expect("Por favor insira um número!");
+
+          println!("Seu palpite foi: {} ", palpite_numero);
+
+          // utiliza o numero_secreto para selecionar um dos arms
+          match palpite_numero.cmp(&numero_secreto) {
+              Ordering::Less => println!("muito baixo!"),
+              Ordering::Greater => println!("muito alto!"),
+              Ordering::Equal => {
+                  println!("Você Venceu!!!");
+                  break;
+              }
+          }
+      }
+}
+
+fn conceitos_basicos() {
     //---Start---conceitos básicos
-    let soma = 32 + 9;
-    let resto = 40 % 6;
-    println!("soma {}\nresto divisão: {}", soma, resto);
+        let soma = 32 + 9;
+        let resto = 40 % 6;
+        println!("soma {}\nresto divisão: {}", soma, resto);
 
-    let tup : (i32, f64, u8) = (500, 6.4, 2);
-        // acesando tupla atravez de destructing
-        let (x, y, z) = tup;
-        println!("\nvalor Z na tupla : {} ", z);
-        println!("\nvalor Y na tupla : {} ", y);
-        println!("\nvalor X na tupla : {} \n", x);
+        let tup : (i32, f64, u8) = (500, 6.4, 2);
+            // acesando tupla atravez de destructing
+            let (x, y, z) = tup;
+            println!("\nvalor Z na tupla : {} ", z);
+            println!("\nvalor Y na tupla : {} ", y);
+            println!("\nvalor X na tupla : {} \n", x);
 
-    // acessando tupla atravez do elemento e index
-    let quinhentos = tup.0;
-    let seis_quatro = tup.1;
-    let dois = tup.2;
-        println!("\nvalor X na tupla : {} \n", quinhentos);
-        println!("\nvalor X na tupla : {} \n", seis_quatro);
-        println!("\nvalor X na tupla : {} \n", dois);
-
-
-
-
-// ---End--- conceitos básicos
-
-/*    // loop para permitir mais de uma única tentativa
-    loop {
-        println!("Digite o palpite de um número : ");
-        let mut palpite_numero = String::new();
-
-        // lidando com possiveis falhas
-        io::stdin()
-            .read_line(&mut palpite_numero)
-            .expect("Falha ao ler a linha");
-
-        // sombrea o palpite_numero e passa o seu valor string para u32
-        // match para lidar com error
-        let palpite_numero: u32 = match palpite_numero.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        // implementação obsoleta com a utilização de expect e crashing par atipos diferentes
-       // let palpite_numero: u32 = palpite_numero.trim().parse().expect("Por favor insira um número!");
-
-        println!("Seu palpite foi: {} ", palpite_numero);
-
-        // utiliza o numero_secreto para selecionar um dos arms
-        match palpite_numero.cmp(&numero_secreto) {
-            Ordering::Less => println!("muito baixo!"),
-            Ordering::Greater => println!("muito alto!"),
-            Ordering::Equal => {
-                println!("Você Venceu!!!");
-                break;
-            }
-        }
-    }    */
+        // acessando tupla atravez do elemento e index
+        let quinhentos = tup.0;
+        let seis_quatro = tup.1;
+        let dois = tup.2;
+            println!("\nvalor X na tupla : {} ", quinhentos);
+            println!("\nvalor X na tupla : {} ", seis_quatro);
+            println!("\nvalor X na tupla : {} \n", dois);
+    //---End--- conceitos básicos
 }
